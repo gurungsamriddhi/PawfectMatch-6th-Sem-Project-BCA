40105bd4f167a3b0b89877e4a55e98ae2fdab406
Samriddhi Gurung <118274160+gurungsamriddhi@users.noreply.github.com>
2025-06-24 18:33:40 +0545

    All routes etup done

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,18 +3,29 @@ class AdminController
 {
     private function loadAdminView($filename)
     {
-        include __DIR__ . '/../../admin/views/' . $filename;
+        include __DIR__ . '/../views/admin/' . $filename;
     }
+
     public function showadminloginform()
     {
-       $this->loadAdminView('adminlogin.php');
+        $this->loadAdminView('adminlogin.php');
     }
+
     public function showdashboard()
     {
+        if (!isset($_SESSION['admin'])) {
+            header("Location: index.php?page=admin/admin_login");
+            exit();
+        }
         $this->loadAdminView('dashboard.php');
     }
+
     public function showaddpetform()
     {
-          $this->loadAdminView('addpet.php');
+        if (!isset($_SESSION['admin'])) {
+            header("Location: index.php?page=admin/admin_login");
+            exit();
+        }
+        $this->loadAdminView('addpet.php');
     }
 }
