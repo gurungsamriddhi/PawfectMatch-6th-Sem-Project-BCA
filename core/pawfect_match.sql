5bff7b265239b5f0eb208358e54fefb5d719830d
Samriddhi Gurung <118274160+gurungsamriddhi@users.noreply.github.com>
2025-07-13 11:40:46 +0545

    sql table improvements

--- core/pawfect_match.sql
+++ core/pawfect_match.sql
@@ -45,35 +45,6 @@ CREATE TABLE pets (
     FOREIGN KEY (posted_by) REFERENCES users(user_id) ON DELETE SET NULL
 );
 
---pets table in samriddhi's database 
-/*CREATE TABLE pets (
-    pet_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    type ENUM('dog', 'cat', 'rabbit', 'other') NOT NULL,
-    breed VARCHAR(100),
-    age DECIMAL(3,1),
-    date_arrival DATE,
-    size ENUM('Small', 'Medium', 'Large', 'Extra Large'),
-    weight DECIMAL(5,2),
-    color VARCHAR(100),
-    characteristics TEXT,
-    gender ENUM('Male', 'Female') NOT NULL,
-    health_status VARCHAR(255),
-    image_path VARCHAR(255),
-    status ENUM('available', 'adopted', 'pending') NOT NULL DEFAULT 'available',
-    posted_by INT,
-    description TEXT,
-    health_notes TEXT,
-    adoption_center VARCHAR(150) NOT NULL,
-    contact_phone VARCHAR(20) NOT NULL,
-    contact_email VARCHAR(100) NOT NULL,
-    center_address TEXT NOT NULL,
-    center_website VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (posted_by) REFERENCES users(user_id) ON DELETE SET NULL
-);*/
-
 -- 3. ADOPTION REQUESTS TABLE
 CREATE TABLE adoption_requests (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -112,6 +83,22 @@ CREATE TABLE wishlist (
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (pet_id) REFERENCES pets(pet_id),
     UNIQUE (user_id, pet_id) -- prevent duplicates
+);
+
+-- 11. adoption_centers Table
+CREATE TABLE adoption_centers (
+    center_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE NOT NULL,      
+    name VARCHAR(150) NOT NULL,
+    established_date DATE,
+    location TEXT,
+    phone VARCHAR(20),
+    number_of_employees INT DEFAULT 0,
+    description TEXT,
+    operating_hours VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 -- 6. VOLUNTEERS TABLE
@@ -176,21 +163,7 @@ CREATE TABLE contact_messages (
 );
 
 
--- 11. adoption_centers Table
-CREATE TABLE adoption_centers (
-    center_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNIQUE NOT NULL,      
-    name VARCHAR(150) NOT NULL,
-    established_date DATE,
-    location TEXT,
-    phone VARCHAR(20),
-    number_of_employees INT DEFAULT 0,
-    description TEXT,
-    operating_hours VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-);
+
 
 
 
